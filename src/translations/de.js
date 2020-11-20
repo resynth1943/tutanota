fa800a25b9020b1abcc7a3f1a50eaf71dde00da3
pas <pas@tutao.de>
2020-11-20 10:15:07 +0100

    New Languages Nov20th

--- src/translations/de.js
+++ src/translations/de.js
@@ -1265,6 +1265,7 @@ module.exports = {
 		"you_label": "Du",
 		"languagePluralCount_label": "{count} Sprachen",
 		"languageSingularCount_label": "{count} Sprache",
-		"languageContentEmpty_label": "Der Inhalt von {language} ist leer!"
+		"languageContentEmpty_label": "Der Inhalt von {language} ist leer!",
+		"templateFilter_label": "Filter... (# um nach Tags zu suchen)"
 	}
 }
